acdf2b8166f8c98d7df14a32ed194a946c73edd8
thuong2606 <nguyenthuongg2606@gmail.com>
2023-07-28 18:58:40 +0700

    first project

---
+++ css/admin/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row headeradmin">
+           <h1>ADMIN</h1> 
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a>Trang chủ</a></li>
+                <li><a>Danh mục</a></li>
+                <li><a>Hàng hóa</a></li>
+                <li><a>khách hàng</a></li>
+                <li><a>Bình luận</a></li>
+                <li><a>Thống kê</a></li>
+            </ul>
+        </div>
---
+++ css/admin/home.php
@@ -0,0 +1,26 @@
+<div class="row">
+            <div class="row formtitle">
+                <h1>Thêm mới loại hàng hóa</h1>
+            </div>
+            <div class="row formcontent">
+                <form action="#" method="post">
+                    <div class="row mb10">
+                        Mã loại<br>
+                    <input type="text" name="maloai">
+
+                    </div>
+                    <div class="row mb10">
+                        Tên loại<br>
+                    <input type="text" name="tenloai">
+
+                    </div>
+                    <input type="submit" value="Thêm mới">
+                    <input type="reset" value="Nhập lại">
+                    <a href="danhsach_loaihang.html"><input type="button" value="Danh sách"></a>
+                </div>
+            </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
